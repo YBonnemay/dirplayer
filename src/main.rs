b32617d74d749d705e612b3ca6aba1be4d4d8129
Yann Bonnemay <y.bonnemay@gmail.com>
2019-04-23 22:39:18 +0200

    Hacky filter on extensions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 // cd /home/bonnemay/git/github/dirplayer/ ; cargo run --verbose
 // ./target/debug/dirplayer
 extern crate termion;
@@ -13,36 +14,19 @@ use std::cmp;
 use std::{fs};
 use std::path::PathBuf;
 use std::fs::DirEntry;
+use std::error::Error;
 
-// use std::process;
-// use std::fs::ReadDir;
 
-// fn list_dir() -> Result<ReadDir> {
-//     let current_dir = env::current_dir()?;
-//     println!(
-//         "Entries modified in the last 24 hours in {:?}:",
-//         current_dir
-//     );
-//     fs::read_dir(current_dir)?
+extern crate serde;
+extern crate serde_json;
 
-//     for entry in fs::read_dir(current_dir)? {
-//         let entry = entry?;
-//         let path = entry.path();
+#[macro_use]
+extern crate serde_derive;
 
-//         let metadata = fs::metadata(&path)?;
-//         let last_modified = metadata.modified()?.elapsed()?.as_secs();
-
-//         if last_modified < 24 * 3600 && metadata.is_file() {
-//             println!(
-//                 "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
-//                 last_modified,
-//                 metadata.permissions().readonly(),
-//                 metadata.len(),
-//                 path.file_name().ok_or("No filename")?
-//             );
-//         }
-//     }
-// }
+#[derive(Deserialize, Debug)]
+struct Config {
+    extensions: Vec< String >,
+}
 
 struct ConstBox {
     input_zone: u16,
@@ -57,25 +41,36 @@ static CONST_BOX: ConstBox = ConstBox {
 pub struct FileList {
     files: Vec<DirEntry>,
     files_number: u16,
+    config: Config,
 }
 
 impl FileList {
-    pub fn set_files(&mut self, current_dir: PathBuf) {
-        for entry in fs::read_dir(current_dir).expect("Unable to list") {
-            match entry {
-                Ok(v) => {
-                    self.files.push(v);
+    pub fn set_files(&mut self, current_dir: PathBuf) -> Result<(), Box<Error>> {
+        for entry in fs::read_dir(current_dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                self.set_files(path).unwrap();
+            } else {
+                let os_extension = path.extension().unwrap();
+                let extension = os_extension.to_str().unwrap();
+                let extension_string = extension.to_string();
+                if self.config.extensions.contains(&extension_string) {
+                    self.files.push(entry);
                 }
-                Err(_) => {}
             }
         }
+        Ok(())
+    }
+
+    pub fn update_files(&mut self, current_dir: PathBuf) -> Result<(), Box<Error>> {
+        self.set_files(current_dir).unwrap();
         self.files_number = self.files.len() as u16;
+        Ok(())
     }
 }
 
-
 fn write_page<W: Write>(starting_line: u16, file_list: &FileList, screen: &mut W)  {
-    println!("{:?} starting_line", starting_line);
     let (_, height) = termion::terminal_size().unwrap();
     let ending_line = starting_line + height - (CONST_BOX.output_zone + CONST_BOX.input_zone);
 
@@ -84,24 +79,6 @@ fn write_page<W: Write>(starting_line: u16, file_list: &FileList, screen: &mut W
         write!(screen, "{}{:?}", termion::cursor::Goto(1, i as u16 + CONST_BOX.input_zone + 1), file_name.path().as_os_str()).unwrap();
     }
 }
-
-// fn get_dirs(current_dir: &PathBuf) -> Vec<DirEntry> {
-//     let mut entries: Vec<DirEntry> = vec![];
-//     for entry in fs::read_dir(current_dir).expect("Unable to list") {
-//         match entry {
-//             Ok(v) => {
-//                 entries.push(v);
-//             }
-//             Err(_) => {}
-//         }
-//     }
-//     (entries)
-// }
-
-// fn entries_to_filenames(entries: Vec<DirEntry>) -> Vec<PathBuf> {
-//     let filenames: Vec<PathBuf> = entries.into_iter().map(|i| i.path()).collect();
-//     (filenames)
-// }
 
 fn write_screen_msg<W: Write>(screen: &mut W, msg: &str) {
     write!(screen, "{}", msg).unwrap();
@@ -115,14 +92,22 @@ fn write_alt_screen_msg<W: Write>(screen: &mut W, file_list: &FileList, current_
     write_page(current_index, &file_list, screen);
 }
 
+fn get_config() -> Config {
+    let json = include_str!("/home/bonnemay/git/github/dirplayer/src/config/config.json");
+    println!("{}", json);
+    serde_json::from_str::<Config>(&json).unwrap()
+}
+
 fn main() {
-    let mut path: PathBuf = [r"/home", "bonnemay"].iter().collect();
+    let config = get_config();
+    let path: PathBuf = [r"/home", "bonnemay", "downloads", "aa_inbox", "Adrien"].iter().collect();
     let mut entries = FileList{
         files: Vec::<DirEntry>::new(),
         files_number: 0u16,
+        config: config,
     };
 
-    entries.set_files(path);
+    entries.update_files(path).unwrap();
     let mut current_index = 0u16;
 
     let mut screen = AlternateScreen::from(stdout().into_raw_mode().unwrap());
