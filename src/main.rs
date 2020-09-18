5d95b92f9fc03035caddf5cad36547378040479a
Yann Bonnemay <y.bonnemay@gmail.com>
2020-09-18 11:41:33 +0200

    Cleans.

--- src/main.rs
+++ src/main.rs
@@ -18,35 +18,22 @@ extern crate serde_derive;
 // ./target/debug/dirplayer
 // println!("write_page{:#?}", lock.len());
 // use termion::screen::{AlternateScreen, ToAlternateScreen, ToMainScreen};
-use std::cmp;
 // use std::io::stdout;
-use walkdir::WalkDir;
-
-use std::sync::{Arc, RwLock};
-use std::thread;
-use std::{
-    error::Error,
-    time::{Duration, Instant},
-};
 
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-
-use std::time;
-use tui::{backend::CrosstermBackend, Terminal};
-
-// use tui::backend::Backend;
+use std::sync::{Arc, RwLock};
+use std::thread;
+use std::time::{Duration, Instant};
 use tui::layout::{Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Modifier, Style};
 use tui::widgets::{Block, Borders, List, Tabs, Text};
 use tui::Frame;
-
-// use crate::demo::{ui, App};
-mod Data;
-mod util;
+use tui::{backend::CrosstermBackend, Terminal};
+use walkdir::WalkDir;
 
 enum Event<I> {
     Input(I),
@@ -66,181 +53,20 @@ fn get_config() -> Config {
     serde_json::from_str::<Config>(&json).unwrap()
 }
 
-pub trait Zone {
-    fn get_zone_offset_start(&self) -> i16;
-    fn get_zone_offset_end(&self) -> i16;
-    fn set_zone_offset_start(&mut self, weight: i16);
-    fn set_zone_offset_end(&mut self, weight: i16);
-
-    fn get_length(&self) -> i16;
-    fn get_lines(&self) -> Vec<String>;
-    // fn use_keystroke(&mut self, _: CEvent);
-}
-
-struct Zones {
-    pub zones: Vec<Box<dyn Zone>>,
-    // pub screen: AlternateScreen<RawTerminal<Stdout>>,
-    pub current_zone: i16,
-}
-
-impl Zones {
-    fn new() -> Zones {
-        Zones {
-            zones: Vec::new(),
-            // screen: screen,
-            current_zone: 0,
-        }
-    }
-
-    fn get_lengths(&self) -> i16 {
-        let zones = &self.zones;
-        let mut lengths = 0;
-        for existing_zone in zones {
-            lengths += existing_zone.get_length();
-        }
-        lengths
-    }
-
-    fn add_zone(&mut self, mut zone: Box<dyn Zone>) {
-        self.zones.push(zone)
-    }
-
-    fn display(&mut self) {}
-
-    // fn flush(&mut self) {
-    //     // self.screen.flush().unwrap();
-    // }
-
-    // fn set_current_zone(&mut self, current_zone: i16) {
-    //     self.current_zone = current_zone
-    // }
-
-    // fn send_keystroke(&mut self, key: CEvent) {
-    //     self.zones[self.current_zone as usize].use_keystroke(key);
-    // }
-}
-
-struct MiddleZone {
-    // https://users.rust-lang.org/t/heartbeat-in-a-thread-done-right/13596
-    x_index: i16,
-    zone_offset_start: i16,
-    zone_offset_end: i16,
-    start: i16,
-    lines: Arc<RwLock<Vec<String>>>,
-    lines_index: i16,
-    // handle: Option<thread::JoinHandle<()>>,
-}
-
-impl MiddleZone {
-    fn new() -> MiddleZone {
-        MiddleZone {
-            x_index: 0,
-            zone_offset_start: 0,
-            zone_offset_end: 0,
-            start: 0,
-            lines: Arc::new(RwLock::new(Vec::new())),
-            lines_index: 0,
-            // handle: None,
-        }
-    }
-
-    // fn refresh_start(&self) {
-    //     let lines = self.lines.clone();
-    //     thread::spawn(move || loop {
-    //         thread::sleep(time::Duration::from_millis(1000));
-    //         let config = get_config();
-    //         let working_directory = config.working_directory.clone();
-
-    //         let new_lines = WalkDir::new(working_directory)
-    //             .into_iter()
-    //             .filter_map(Result::ok)
-    //             .map(|e| String::from(e.file_name().to_string_lossy()))
-    //             .collect::<Vec<String>>();
-
-    //         let mut writable_lines = lines.write().unwrap();
-    //         *writable_lines = new_lines;
-    //     });
-    // }
-}
-
-impl Zone for MiddleZone {
-    fn get_zone_offset_start(&self) -> i16 {
-        self.zone_offset_start
-    }
-
-    fn set_zone_offset_start(&mut self, zone_offset_start: i16) {
-        self.zone_offset_start = zone_offset_start + 1;
-    }
-
-    fn get_zone_offset_end(&self) -> i16 {
-        self.zone_offset_end
-    }
-
-    fn set_zone_offset_end(&mut self, zone_offset_end: i16) {
-        self.zone_offset_end = zone_offset_end;
-    }
-
-    fn get_length(&self) -> i16 {
-        // let terminal_size = termion::terminal_size().unwrap();
-        // terminal_size.1 as i16 - self.get_zone_offset_start() + self.get_zone_offset_end()
-        return 1;
-    }
-
-    fn get_lines(&self) -> Vec<String> {
-        // let lines = self.lines.read().unwrap();
-        // return lines.to_vec();
-        let lines = self.lines.read().unwrap().to_vec();
-        let lines_len = lines.len();
-        let lines_index = self.lines_index as usize;
-        let lines_start = cmp::min(lines_len, lines_index);
-        let lines_end = cmp::min(lines_len, lines_index + self.get_length() as usize);
-
-        // println!("lines_start {:#?}", lines_start);
-        // println!("lines_end {:#?}", lines_end);
-        // println!("self.get_length() {:#?}", self.get_length());
-
-        return (lines[lines_start as usize..lines_end as usize]).to_vec();
-    }
-
-    // fn use_keystroke(&mut self, key: CEvent) {
-    //     match key {
-    //         CEvent::Down => {
-    //             self.lines_index = self.lines_index + 1;
-    //         }
-    //         CEvent::Up => {
-    //             if self.lines_index > 0 {
-    //                 self.lines_index = self.lines_index - 1;
-    //             }
-    //         }
-    //         _ => {}
-    //     }
-    // }
-}
-
-// use std::io::Write::flush as Wflush;
-
 use std::io::{stdout, Write};
 
 use crossbeam_channel::unbounded;
 use notify::{watcher, RecursiveMode, Watcher};
-use std::path::Path;
 use std::path::PathBuf;
-// use std::sync::mpsc::channel;
-
-// A DataSource represents data that will de displayed. A DirectoryDataSource is a watched directory
-// A Displayer
 
 pub trait DataSource<T> {
     fn get_lines(&self) -> Vec<T>;
 }
 
-// THis will represent a Directory for us
-
 struct Directory {
     path: Arc<RwLock<PathBuf>>,
-    watcher: notify::INotifyWatcher,
-    walk_dir: WalkDir,
-    sender: crossbeam_channel::Sender<std::result::Result<notify::Event, notify::Error>>,
+    _watcher: notify::INotifyWatcher,
+    _sender: crossbeam_channel::Sender<std::result::Result<notify::Event, notify::Error>>,
     receiver: crossbeam_channel::Receiver<std::result::Result<notify::Event, notify::Error>>,
     lines: Arc<RwLock<Vec<String>>>,
 }
@@ -248,7 +74,6 @@ struct Directory {
 impl Directory {
     fn new(pathbuf: PathBuf) -> Directory {
         let (sender, receiver) = unbounded();
-        // Automatically select the best implementation for your platform.
         let mut watcher = watcher(sender.clone(), Duration::from_secs(1)).unwrap();
 
         let path = Arc::new(RwLock::new(pathbuf.clone()));
@@ -259,22 +84,18 @@ impl Directory {
 
         Directory {
             path,
-            watcher,
-            walk_dir: WalkDir::new(pathbuf),
-            sender: sender,
+            _sender: sender,
+            _watcher: watcher,
             receiver,
             lines: Arc::new(RwLock::new(Vec::new())),
         }
     }
 
-    // fn listen(self) {}
-
-    fn refresh_lines(&self) {
-        let lines = self.lines.clone();
-        let path = self.path.clone();
-
+    fn get_new_lines(
+        path: &std::sync::Arc<std::sync::RwLock<std::path::PathBuf>>,
+        lines: &std::sync::Arc<std::sync::RwLock<std::vec::Vec<std::string::String>>>,
+    ) {
         let path_read = &*(path.read().unwrap());
-        // let new_path = &*path_read;
         let new_lines = WalkDir::new(path_read)
             .into_iter()
             .filter_map(|e| e.ok())
@@ -285,29 +106,25 @@ impl Directory {
         *writable_lines = new_lines;
     }
 
+    fn refresh_lines(&self) {
+        let path = self.path.clone();
+        let lines = self.lines.clone();
+        Directory::get_new_lines(&path, &lines);
+    }
+
     fn listen(&self) {
-        // Listening to changes.
         let receiver = self.receiver.clone();
         let lines = self.lines.clone();
         let path = self.path.clone();
 
+        // Wait here for changes
         thread::spawn(move || loop {
             match receiver.recv() {
-                Ok(event) => {
-                    let path_read = &*(path.read().unwrap());
-                    let new_lines = WalkDir::new(path_read)
-                        .into_iter()
-                        .filter_map(|e| e.ok())
-                        .map(|e| String::from(e.file_name().to_string_lossy()))
-                        .collect::<Vec<String>>();
-                    let mut writable_lines = lines.write().unwrap();
-                    *writable_lines = new_lines;
-
-                    // println!("{:?}", event);
+                Ok(_) => {
+                    Directory::get_new_lines(&path, &lines);
                 }
                 Err(e) => println!("watch error: {:?}", e),
             };
-            // println!("thread loopng");
         });
     }
 }
@@ -334,29 +151,14 @@ fn draw<B: tui::backend::Backend>(f: &mut Frame<B>, display_data: &mut DisplayDa
     let text_lines = displayable_lines.iter().map(|e| Text::raw(e));
     let text_lines = List::new(text_lines);
 
-    // let displayable_lines = display_data.directory.lines.read().unwrap();
-    // let text_lines = displayable_lines.iter().map(|e| Text::raw(e));
-    // let text_lines = List::new(text_lines);
-
     f.render_widget(text_lines, chunks[0])
-    // lines.iter().map(|e| Text::raw(e)).collect()
-
-    // let new_lines = WalkDir::new(working_directory)
-    //     .into_iter()
-    //     .filter_map(Result::ok)
-    //     .map(|e| String::from(e.file_name().to_string_lossy()))
-    //     .collect::<Vec<String>>();
-
-    // .map(|e| tui::widgets::Text::raw(e.file_name().to_string_lossy()));
-
-    // let tasks = List::new(lines);
-    // f.render_widget(tasks, chunks[0]);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Start
-    let mut directory = Directory::new(PathBuf::from("/home/bonnemay/tests/src"));
+    let directory = Directory::new(PathBuf::from("/home/bonnemay/tests/src"));
     directory.refresh_lines();
+    println!("LISTEN CALL");
     directory.listen();
     let mut display_data = DisplayData::new(directory);
 
