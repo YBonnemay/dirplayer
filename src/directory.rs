d09c6a04b4617219421fa6a554895208d190fb00
Yann Bonnemay <y.bonnemay@gmail.com>
2020-10-11 18:14:53 +0200

    Cleans

---
+++ src/directory.rs
@@ -0,0 +1,77 @@
+use crossbeam_channel::unbounded;
+use notify::{watcher, RecursiveMode, Watcher};
+use std::path::PathBuf;
+use std::sync::{Arc, RwLock};
+use std::thread;
+use std::time::Duration;
+use walkdir::WalkDir;
+
+// This represents an updated view on a directory.
+// It makes the recursive contents of a directory available and updated.
+// For that is waits for notify::Watcher events
+
+pub struct Directory {
+    pub path: Arc<RwLock<PathBuf>>,
+    pub _watcher: notify::INotifyWatcher,
+    pub _sender: crossbeam_channel::Sender<std::result::Result<notify::Event, notify::Error>>,
+    pub receiver: crossbeam_channel::Receiver<std::result::Result<notify::Event, notify::Error>>,
+    pub lines: Arc<RwLock<Vec<String>>>,
+}
+
+impl Directory {
+    pub fn new(pathbuf: PathBuf) -> Directory {
+        let (sender, receiver) = unbounded();
+        let mut watcher = watcher(sender.clone(), Duration::from_secs(1)).unwrap();
+
+        let path = Arc::new(RwLock::new(pathbuf.clone()));
+        // Watching directory.
+        watcher
+            .watch(pathbuf.clone(), RecursiveMode::Recursive)
+            .unwrap();
+
+        Directory {
+            path,
+            _sender: sender,
+            _watcher: watcher,
+            receiver,
+            lines: Arc::new(RwLock::new(Vec::new())),
+        }
+    }
+
+    pub fn get_new_lines(
+        path: &std::sync::Arc<std::sync::RwLock<std::path::PathBuf>>,
+        lines: &std::sync::Arc<std::sync::RwLock<std::vec::Vec<std::string::String>>>,
+    ) {
+        let path_read = &*(path.read().unwrap());
+        let new_lines = WalkDir::new(path_read)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .map(|e| String::from(e.file_name().to_string_lossy()))
+            .collect::<Vec<String>>();
+
+        let mut writable_lines = lines.write().unwrap();
+        *writable_lines = new_lines;
+    }
+
+    pub fn refresh_lines(&self) {
+        let path = self.path.clone();
+        let lines = self.lines.clone();
+        Directory::get_new_lines(&path, &lines);
+    }
+
+    pub fn listen(&self) {
+        let receiver = self.receiver.clone();
+        let lines = self.lines.clone();
+        let path = self.path.clone();
+
+        // Wait here for changes
+        thread::spawn(move || loop {
+            match receiver.recv() {
+                Ok(_) => {
+                    Directory::get_new_lines(&path, &lines);
+                }
+                Err(e) => println!("watch error: {:?}", e),
+            };
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,18 @@ extern crate walkdir;
 #[macro_use]
 extern crate serde_derive;
 
-// reale time updating of the data
+// real time updating of the data
 
-// cd /home/bonnemay/github/tui-rs/example/ ; cargo run --verbose
 // cd /home/bonnemay/github/dirplayer/ ; cargo run --verbose
 // cd /home/bonnemay/github/dirplayer/ ; RUST_BACKTRACE=1 cargo run --verbose
-// ./target/debug/dirplayer
 // println!("write_page{:#?}", lock.len());
-// use termion::screen::{AlternateScreen, ToAlternateScreen, ToMainScreen};
-// use std::io::stdout;
 
+use crossbeam_channel::unbounded;
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::sync::{Arc, RwLock};
 use std::thread;
 use std::time::{Duration, Instant};
 use tui::layout::{Constraint, Direction, Layout, Rect};
@@ -33,7 +29,6 @@ use tui::style::{Color, Modifier, Style};
 use tui::widgets::{Block, Borders, List, Tabs, Text};
 use tui::Frame;
 use tui::{backend::CrosstermBackend, Terminal};
-use walkdir::WalkDir;
 
 enum Event<I> {
     Input(I),
@@ -55,78 +50,13 @@ fn get_config() -> Config {
 
 use std::io::{stdout, Write};
 
-use crossbeam_channel::unbounded;
-use notify::{watcher, RecursiveMode, Watcher};
 use std::path::PathBuf;
+
+mod directory;
+use directory::Directory;
 
 pub trait DataSource<T> {
     fn get_lines(&self) -> Vec<T>;
-}
-
-struct Directory {
-    path: Arc<RwLock<PathBuf>>,
-    _watcher: notify::INotifyWatcher,
-    _sender: crossbeam_channel::Sender<std::result::Result<notify::Event, notify::Error>>,
-    receiver: crossbeam_channel::Receiver<std::result::Result<notify::Event, notify::Error>>,
-    lines: Arc<RwLock<Vec<String>>>,
-}
-
-impl Directory {
-    fn new(pathbuf: PathBuf) -> Directory {
-        let (sender, receiver) = unbounded();
-        let mut watcher = watcher(sender.clone(), Duration::from_secs(1)).unwrap();
-
-        let path = Arc::new(RwLock::new(pathbuf.clone()));
-        // Watching directory.
-        watcher
-            .watch(pathbuf.clone(), RecursiveMode::Recursive)
-            .unwrap();
-
-        Directory {
-            path,
-            _sender: sender,
-            _watcher: watcher,
-            receiver,
-            lines: Arc::new(RwLock::new(Vec::new())),
-        }
-    }
-
-    fn get_new_lines(
-        path: &std::sync::Arc<std::sync::RwLock<std::path::PathBuf>>,
-        lines: &std::sync::Arc<std::sync::RwLock<std::vec::Vec<std::string::String>>>,
-    ) {
-        let path_read = &*(path.read().unwrap());
-        let new_lines = WalkDir::new(path_read)
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .map(|e| String::from(e.file_name().to_string_lossy()))
-            .collect::<Vec<String>>();
-
-        let mut writable_lines = lines.write().unwrap();
-        *writable_lines = new_lines;
-    }
-
-    fn refresh_lines(&self) {
-        let path = self.path.clone();
-        let lines = self.lines.clone();
-        Directory::get_new_lines(&path, &lines);
-    }
-
-    fn listen(&self) {
-        let receiver = self.receiver.clone();
-        let lines = self.lines.clone();
-        let path = self.path.clone();
-
-        // Wait here for changes
-        thread::spawn(move || loop {
-            match receiver.recv() {
-                Ok(_) => {
-                    Directory::get_new_lines(&path, &lines);
-                }
-                Err(e) => println!("watch error: {:?}", e),
-            };
-        });
-    }
 }
 
 struct DisplayData {
@@ -158,7 +88,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Start
     let directory = Directory::new(PathBuf::from("/home/bonnemay/tests/src"));
     directory.refresh_lines();
-    println!("LISTEN CALL");
     directory.listen();
     let mut display_data = DisplayData::new(directory);
 
@@ -222,6 +151,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 // data.on_tick();
             }
         }
+
         // if data.should_quit {
         //     break;
         // }
